2e419c5a37634bb700d5feb65a2ca3e1a1791fff
michalliss <michal.j.liss@gmail.com>
2026-05-26 21:05:43 +0200

    works

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,4 @@
-FROM php:8.5.6
+FROM php:8.2.12-apache
+RUN docker-php-ext-install mysqli
+RUN docker-php-ext-enable mysqli
 COPY htdocs/ /var/www/html/
--- htdocs/index.php
+++ htdocs/index.php
@@ -4,7 +4,12 @@ require_once('hello.php');
 
 echo hello(" world!");
 
-$db = new mysqli("localhost", "root", "", "memy");
+$db_host = getenv('DB_HOST') ?: "localhost";
+$db_username = getenv('DB_USER') ?: "root";
+$db_password = getenv('DB_PASSWORD') ?: "";
+$db_database = getenv('DB_DATABASE') ?: "memy";
+$db_port = intval(getenv('DB_PORT')) ?: 3306;
+$db = new mysqli($db_host, $db_username, $db_password, $db_database, $db_port);
 $query = "SELECT * FROM memy";
 $result = $db->query($query);
 
